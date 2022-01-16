c85134aa60649e16cc4246f88c38d3632d8d1227
Vendy Masarová <vendymasarova@seznam.cz>
2022-01-16 20:00:40 +0100

    sass architecture

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,27 @@
 const nav = document.querySelector(".nav");
-const login = document.querySelector(".login");
+const login = document.querySelector(".sidebar");
 const openBtnNav = document.querySelector(".nav__open");
-const openBtnLogin = document.querySelector(".login__open");
+const openBtnNavImg = document.querySelector(".nav__open--img");
+const openBtnLogin = document.querySelector(".sidebar__open");
+const openBtnLoginImg = document.querySelector(".sidebar__open--img");
 const arrow = document.querySelector(".nav__open img");
+
+//NAVIGATION
 openBtnNav.addEventListener("click", () => {
   nav.classList.toggle("open");
+  if (nav.classList.contains("open")) {
+    openBtnNavImg.style.transform = "rotate(0)";
+  } else {
+    openBtnNavImg.style.transform = "rotate(180deg)";
+  }
 });
+
+//SIDEBAR
 openBtnLogin.addEventListener("click", () => {
   login.classList.toggle("open");
+  if (login.classList.contains("open")) {
+    openBtnLoginImg.style.transform = "rotate(180deg)";
+  } else {
+    openBtnLoginImg.style.transform = "rotate(0)";
+  }
 });
